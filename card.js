e35230c0af5c19fedc283cc5659a4aedfaf2a89b
Tammy Albin <lestertammy1977@yahoo.com>
2026-08-12 16:54:58 -0500

    random closing on the page, closes #3

--- card.js
+++ card.js
@@ -4,6 +4,19 @@ let nameInput = document.getElementById("name-input");
 let adjectiveInput = document.getElementById("adjective-input");
 let nounInput = document.getElementById("noun-input");
 let cardText = document.getElementById("card-text");
+let closingText = document.getElementById("closing-text");
+
+let closings = [
+  "Warmly,",
+  "With love,",
+  "Best wishes,",
+  "Take care,",
+  "Your friend,"
+];
+
+function randomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
 
 function makeCard() {
   let friendName = nameInput.value;
@@ -11,6 +24,9 @@ function makeCard() {
   let noun = nounInput.value;
 
   cardText.textContent = `Dear ${friendName}, you are a ${adjective} ${noun}!`;
+
+  let closing = closings[randomNumber(0, closings.length - 1)];
+  closingText.textContent = closing;
 }
 
 makeButton.addEventListener("click", makeCard);
