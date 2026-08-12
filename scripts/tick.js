a99b9b8cc91079ab09e77000e7d85cfa389082d8
Tammy Albin <lestertammy1977@yahoo.com>
2026-08-11 22:10:44 -0500

    speed and stop experiments

--- scripts/tick.js
+++ scripts/tick.js
@@ -1,10 +1,15 @@
 
 let count = 0;
+let speed = 1000;
 
 function tick() {
   count = count + 1;
   console.log("Tick " + count);
+
+if (count === 10) {
+    clearInterval(timer);
+    }
 }
 
-setInterval(tick, 1000);
+let timer = setInterval(tick, speed);
 
