d6217abdc3caca43e0431faf020756327329d85f
Tammy Albin <lestertammy1977@yahoo.com>
2026-08-11 21:38:26 -0500

    the generator ran itself

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -1,10 +1,14 @@
 let name = prompt("A name:");
 let adjective = prompt("An adjective:");
 let noun = prompt("A noun:");
+let place = prompt("A place:"); 
+let food = prompt("A food:");
 
 function makeCard() {
     console.log("Dear " + name + ",");
     console.log("You are as " + adjective + " as a " +      noun + ".");
+    console.log("I hope you have a wonderful time at "      + place + "."); 
+    console.log("Enjoy some " + food + " on your special day!");
     console.log("Happy birthday from the generator.");
 }
 
